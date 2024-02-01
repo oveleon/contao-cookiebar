65c7a6585b957985f65a09b9df95b4cf3d390a6e
xprojects-de <infos@x-projects.de>
2024-02-01 10:58:12 +0100

    bugfixes

--- src/EventListener/KernelRequestListener.php
+++ src/EventListener/KernelRequestListener.php
@@ -43,25 +43,26 @@ class KernelRequestListener
         $request = $event->getRequest();
 
         if (
-            $this->rootPageBuffer === null &&
-            $this->scopeMatcher->isFrontendRequest($request) &&
-            $request->attributes->has('pageModel')
+            $this->rootPageBuffer !== null ||
+            $this->scopeMatcher->isFrontendRequest($request) === false ||
+            !$request->attributes->has('pageModel')
         )
         {
-            // use first request of any Contao-request to handle the pageModel settings
-            // because every Contao-Request does have the PageModel
-            // @TODO maybe it can be moved to ResponseListener at first place
-            $pageModel = $request->attributes->get('pageModel');
-            if ($pageModel instanceof PageModel)
+            return;
+        }
+
+        // use first request of any Contao-request to handle the pageModel settings
+        // because every Contao-Request does have the PageModel
+        // @TODO maybe it can be moved to ResponseListener at first place
+        $pageModel = $request->attributes->get('pageModel');
+        if ($pageModel instanceof PageModel)
+        {
+            $this->objPage = $pageModel;
+
+            $rootPageObject = PageModel::findByPk($pageModel->rootId);
+            if ($rootPageObject instanceof PageModel)
             {
-                $this->objPage = $pageModel;
-
-                $rootPageObject = PageModel::findByPk($pageModel->rootId);
-                if ($rootPageObject instanceof PageModel)
-                {
-                    $this->prepareCookieBar($pageModel, $rootPageObject);
-                }
-
+                $this->prepareCookieBar($pageModel, $rootPageObject);
             }
 
         }
@@ -77,61 +78,62 @@ class KernelRequestListener
         $request = $event->getRequest();
 
         if (
-            $this->scopeMatcher->isFrontendRequest($request) &&
-            $request->attributes->has('pageModel')
+            $this->scopeMatcher->isFrontendRequest($request) === false ||
+            !$request->attributes->has('pageModel')
         )
         {
-            $response = $event->getResponse();
-            $content = $response->getContent();
+            return;
+        }
 
-            // normal Website only have one Main-Request for fe_page
-            // Ajax-requests do not have a Root-Page so CookieBar do not work
-            $pageModel = $request->attributes->get('pageModel');
-            if (
-                ($pageModel instanceof PageModel) &&
-                $this->isPageTemplate($event) === true
-            )
+        $response = $event->getResponse();
+        $content = $response->getContent();
+
+        // normal Website only have one Main-Request for fe_page
+        // Ajax-requests do not have a Root-Page so CookieBar do not work
+        $pageModel = $request->attributes->get('pageModel');
+        if (
+            ($pageModel instanceof PageModel) &&
+            $this->isPageTemplate($event) === true
+        )
+        {
+            $content = match ($this->rootPagePosition)
             {
-                $content = match ($this->rootPagePosition)
-                {
-                    'bodyAboveContent' => preg_replace("/<body([^>]*)>(.*?)<\/body>/is", "<body$1>$this->rootPageBuffer$2</body>", $content),
-                    default => str_replace("</body>", "$this->rootPageBuffer</body>", $content),
-                };
+                'bodyAboveContent' => preg_replace("/<body([^>]*)>(.*?)<\/body>/is", "<body$1>$this->rootPageBuffer$2</body>", $content),
+                default => str_replace("</body>", "$this->rootPageBuffer</body>", $content),
+            };
 
-                $content = $this->injectGlobalJs($content);
-                $response->setContent($content);
+            $content = $this->injectGlobalJs($content);
+            $response->setContent($content);
 
+        }
+
+        if ($request->attributes->has('contentModel'))
+        {
+            $contentModel = $request->attributes->get('contentModel');
+
+            if (!$contentModel instanceof ContentModel)
+            {
+                $contentModel = ContentModel::findByPk($contentModel);
             }
 
-            if ($request->attributes->has('contentModel'))
+            // renew $content because using insertTags in modules it could be that contentModel and moduleModel is set
+            $content = $this->parseTemplates($contentModel, $content);
+            $response->setContent($content);
+
+        }
+
+        if ($request->attributes->has('moduleModel'))
+        {
+            $moduleModel = $request->attributes->get('moduleModel');
+
+            if (!$moduleModel instanceof ModuleModel)
             {
-                $contentModel = $request->attributes->get('contentModel');
-
-                if (!$contentModel instanceof ContentModel)
-                {
-                    $contentModel = ContentModel::findByPk($contentModel);
-                }
-
-                // renew $content because using insertTags in modules it could be that contentModel and moduleModel is set
-                $content = $this->parseTemplates($contentModel, $content);
-                $response->setContent($content);
-
+                $moduleModel = ModuleModel::findByPk($moduleModel);
             }
 
-            if ($request->attributes->has('moduleModel'))
-            {
-                $moduleModel = $request->attributes->get('moduleModel');
-
-                if (!$moduleModel instanceof ModuleModel)
-                {
-                    $moduleModel = ModuleModel::findByPk($moduleModel);
-                }
-
-                // renew $content because using insertTags in modules it could be that contentModel and moduleModel is set
-                $content = $this->parseTemplates($moduleModel, $content);
-                $response->setContent($content);
-
-            }
+            // renew $content because using insertTags in modules it could be that contentModel and moduleModel is set
+            $content = $this->parseTemplates($moduleModel, $content);
+            $response->setContent($content);
 
         }
 
@@ -145,41 +147,43 @@ class KernelRequestListener
     private function prepareCookieBar(PageModel $pageModel, PageModel $rootPageModel): void
     {
         $objConfig = Cookiebar::getConfigByPage($rootPageModel);
-        if ($objConfig !== null)
+
+        if (!$objConfig instanceof CookiebarModel)
         {
-            $this->cookiebarModel = $objConfig;
-
-            $strHtml = Cookiebar::parseCookiebarTemplate($objConfig);
-
-            if ((string)$objConfig->scriptPosition === 'body')
-            {
-                $strHtml .= '<script src="bundles/contaocookiebar/scripts/cookiebar.min.js"></script>';
-            } else
-            {
-                // @TODO better implementation
-                $this->globalCss = '<script src="/bundles/contaocookiebar/scripts/cookiebar.min.js?v=' . time() . '"></script>';
-                // $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/contaocookiebar/scripts/cookiebar.min.js|static';
-            }
-
-            $strHtml .= vsprintf("<script>var cookiebar = new ContaoCookiebar({configId:%s,pageId:%s,version:%s,lifetime:%s,token:'%s',doNotTrack:%s,currentPageId:%s,excludedPageIds:%s,cookies:%s,configs:%s,disableTracking:%s, texts:{acceptAndDisplay:'%s'}});</script>", [
-                $objConfig->id,
-                $objConfig->pageId,
-                $objConfig->version,
-                System::getContainer()->getParameter('contao_cookiebar.lifetime'),
-                System::getContainer()->getParameter('contao_cookiebar.storage_key'),
-                System::getContainer()->getParameter('contao_cookiebar.consider_dnt') ? 1 : 0,
-                $pageModel->id,
-                json_encode(StringUtil::deserialize($objConfig->excludePages)),
-                json_encode(Cookiebar::validateCookies($objConfig)),
-                json_encode(Cookiebar::validateGlobalConfigs($objConfig)),
-                $this->tokenChecker->hasBackendUser() && !!$objConfig->disableTrackingWhileLoggedIn ? 1 : 0,
-                $this->translator->trans('tl_cookiebar.acceptAndDisplayLabel', [], 'contao_default')
-            ]);
-
-            $this->rootPageBuffer = $strHtml;
-            $this->rootPagePosition = $objConfig->position;
-
+            return;
         }
+
+        $this->cookiebarModel = $objConfig;
+
+        $strHtml = Cookiebar::parseCookiebarTemplate($objConfig);
+
+        if ((string)$objConfig->scriptPosition === 'body')
+        {
+            $strHtml .= '<script src="bundles/contaocookiebar/scripts/cookiebar.min.js"></script>';
+        } else
+        {
+            // @TODO better implementation
+            $this->globalCss = '<script src="/bundles/contaocookiebar/scripts/cookiebar.min.js?v=' . time() . '"></script>';
+            // $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/contaocookiebar/scripts/cookiebar.min.js|static';
+        }
+
+        $strHtml .= vsprintf("<script>var cookiebar = new ContaoCookiebar({configId:%s,pageId:%s,version:%s,lifetime:%s,token:'%s',doNotTrack:%s,currentPageId:%s,excludedPageIds:%s,cookies:%s,configs:%s,disableTracking:%s, texts:{acceptAndDisplay:'%s'}});</script>", [
+            $objConfig->id,
+            $objConfig->pageId,
+            $objConfig->version,
+            System::getContainer()->getParameter('contao_cookiebar.lifetime'),
+            System::getContainer()->getParameter('contao_cookiebar.storage_key'),
+            System::getContainer()->getParameter('contao_cookiebar.consider_dnt') ? 1 : 0,
+            $pageModel->id,
+            json_encode(StringUtil::deserialize($objConfig->excludePages)),
+            json_encode(Cookiebar::validateCookies($objConfig)),
+            json_encode(Cookiebar::validateGlobalConfigs($objConfig)),
+            $this->tokenChecker->hasBackendUser() && !!$objConfig->disableTrackingWhileLoggedIn ? 1 : 0,
+            $this->translator->trans('tl_cookiebar.acceptAndDisplayLabel', [], 'contao_default')
+        ]);
+
+        $this->rootPageBuffer = $strHtml;
+        $this->rootPagePosition = $objConfig->position;
 
     }
 
@@ -205,51 +209,52 @@ class KernelRequestListener
         $arrTypes = Cookiebar::getIframeTypes();
         $arrCookies = Cookiebar::validateCookies($objConfig);
 
-        if (is_array($arrTypes))
+        if (!is_array($arrTypes))
         {
-            foreach ($arrTypes as $strType => $arrTemplates)
+            return $buffer;
+        }
+
+        foreach ($arrTypes as $strType => $arrTemplates)
+        {
+            if (in_array($template, $arrTemplates))
             {
-                if (in_array($template, $arrTemplates))
+                foreach ($arrCookies as $cookie)
                 {
-                    foreach ($arrCookies as $cookie)
+                    if (isset($cookie['iframeType']) && $cookie['iframeType'] === $strType)
                     {
-                        if (isset($cookie['iframeType']) && $cookie['iframeType'] === $strType)
+                        $strBlockUrl = '/cookiebar/block/' . $this->objPage->language . '/' . $cookie['id'] . '?redirect=';
+
+                        // Check if the element is delivered with a preview image
+                        if (strpos($buffer, 'id="splashImage') !== false)
                         {
-                            $strBlockUrl = '/cookiebar/block/' . $this->objPage->language . '/' . $cookie['id'] . '?redirect=';
+                            // Regex: Modify href attribute for splash images
+                            $atagRegex = "/id=\"splashImage_([^>]*)href=\"([^>]*)\"/is";
 
-                            // Check if the element is delivered with a preview image
-                            if (strpos($buffer, 'id="splashImage') !== false)
+                            // Get current href attribute
+                            preg_match($atagRegex, $buffer, $matches);
+
+                            // Overwrite href attribute
+                            $buffer = preg_replace($atagRegex, 'id="splashImage_$1href="' . $strBlockUrl . urlencode($matches[2]) . '"', $buffer);
+                            $buffer = str_replace('iframe.src', 'iframe.setAttribute("data-ccb-id", "' . $cookie['id'] . '"); iframe.src', $buffer);
+
+                        } else
+                        {
+                            // Regex: Modify src attribute for iframes
+                            $frameRegex = "/<iframe([\s\S]*?)src=([\\\\\"\']+)(.*?)[\\\\\"\']+/i";
+
+                            // Get current src attribute
+                            preg_match_all($frameRegex, $buffer, $matches);
+
+                            $matchCount = count($matches[0]);
+                            for ($i = 0; $i < $matchCount; $i++)
                             {
-                                // Regex: Modify href attribute for splash images
-                                $atagRegex = "/id=\"splashImage_([^>]*)href=\"([^>]*)\"/is";
 
-                                // Get current href attribute
-                                preg_match($atagRegex, $buffer, $matches);
+                                $quote = $matches[2][$i];
+                                $search = 'src=' . $quote;
+                                $replace = 'data-ccb-id=' . $quote . $cookie['id'] . $quote . '  src=' . $quote . $strBlockUrl . urlencode($matches[3][$i]) . $quote . ' data-src=' . $quote;
 
-                                // Overwrite href attribute
-                                $buffer = preg_replace($atagRegex, 'id="splashImage_$1href="' . $strBlockUrl . urlencode($matches[2]) . '"', $buffer);
-                                $buffer = str_replace('iframe.src', 'iframe.setAttribute("data-ccb-id", "' . $cookie['id'] . '"); iframe.src', $buffer);
-
-                            } else
-                            {
-                                // Regex: Modify src attribute for iframes
-                                $frameRegex = "/<iframe([\s\S]*?)src=([\\\\\"\']+)(.*?)[\\\\\"\']+/i";
-
-                                // Get current src attribute
-                                preg_match_all($frameRegex, $buffer, $matches);
-
-                                $matchCount = count($matches[0]);
-                                for ($i = 0; $i < $matchCount; $i++)
-                                {
-
-                                    $quote = $matches[2][$i];
-                                    $search = 'src=' . $quote;
-                                    $replace = 'data-ccb-id=' . $quote . $cookie['id'] . $quote . '  src=' . $quote . $strBlockUrl . urlencode($matches[3][$i]) . $quote . ' data-src=' . $quote;
-
-                                    $iframe = str_replace($search, $replace, $matches[0][$i]);
-                                    $buffer = str_replace($matches[0][$i], $iframe, $buffer);
-
-                                }
+                                $iframe = str_replace($search, $replace, $matches[0][$i]);
+                                $buffer = str_replace($matches[0][$i], $iframe, $buffer);
 
                             }
 
@@ -257,9 +262,9 @@ class KernelRequestListener
 
                     }
 
-                    break;
-
                 }
+
+                break;
 
             }
 
