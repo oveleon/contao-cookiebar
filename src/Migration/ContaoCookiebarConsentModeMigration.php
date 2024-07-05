da20afa80cc261d5b30366c383b825ad3529b882
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-07-05 12:03:51 +0200

    Update consent mode migration

--- src/Migration/ContaoCookiebarConsentModeMigration.php
+++ src/Migration/ContaoCookiebarConsentModeMigration.php
@@ -5,6 +5,7 @@ namespace Oveleon\ContaoCookiebar\Migration;
 use Contao\CoreBundle\Migration\AbstractMigration;
 use Contao\CoreBundle\Migration\MigrationResult;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
 
 class ContaoCookiebarConsentModeMigration extends AbstractMigration
 {
@@ -23,26 +24,41 @@ class ContaoCookiebarConsentModeMigration extends AbstractMigration
         return 'Contao Cookiebar: Consent Mode Migration';
     }
 
+    /**
+     * @throws Exception
+     */
     public function shouldRun(): bool
     {
-        $schemaManager = $this->connection->getSchemaManager();
+        $schemaManager = method_exists(Connection::class, 'createSchemaManager') ? $this->connection->createSchemaManager() : $this->connection->getSchemaManager();
 
-        if (!$schemaManager->tablesExist(['tl_cookie'])) {
+        if (!$schemaManager->tablesExist(['tl_cookie']))
+        {
             return false;
         }
 
-        return $this->connection->fetchOne("SELECT TRUE FROM tl_cookie WHERE type = 'googleConsentMode' AND NOT gcmMode = '' AND gcmMode NOT LIKE 'a:%'");
+        $columns = $schemaManager->listTableColumns('tl_cookie');
+
+        if (!isset($columns['gcmmode']))
+        {
+            return false;
+        }
+
+        $fieldConversion = 32 === $columns['gcmmode']->getLength();
+        $valueConversion = $this->connection->fetchOne("SELECT TRUE FROM tl_cookie WHERE type = 'googleConsentMode' AND NOT gcmMode = '' AND gcmMode NOT LIKE 'a:%'");
+
+        return $fieldConversion || $valueConversion;
     }
 
     public function run(): MigrationResult
     {
+        $this->connection->executeQuery("ALTER TABLE tl_cookie CHANGE gcmMode gcmMode VARCHAR(255) DEFAULT '' NOT NULL");
+
         $values = $this->connection->fetchAllKeyValue("SELECT id, gcmMode FROM tl_cookie WHERE type = 'googleConsentMode' AND NOT gcmMode = '' AND gcmMode NOT LIKE 'a:%'");
 
         foreach ($values as $id => $value)
         {
             $this->connection->update('tl_cookie', ['gcmMode' => serialize([$value])], ['id' => (int) $id]);
         }
-
 
         return $this->createResult(true, 'Old consent mode option have been serialized.');
     }
