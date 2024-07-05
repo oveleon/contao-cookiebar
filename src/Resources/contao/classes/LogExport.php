2aa972ce22f7be290a3c1850b82326a3352791dc
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-07-05 14:45:24 +0200

    Improved log export

--- src/Resources/contao/classes/LogExport.php
+++ src/Resources/contao/classes/LogExport.php
@@ -11,66 +11,113 @@
 namespace Oveleon\ContaoCookiebar;
 
 use Contao\Config;
+use Contao\Database;
 use Contao\File;
 use Contao\StringUtil;
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Serializer\Encoder\CsvEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Contao\System;
+use Exception;
 
 class LogExport
 {
     /**
      * Export log
      *
-     * @throws \Exception
+     * @throws Exception
      */
-    public function export()
+    public function export(): void
     {
-        $objLog = CookieLogModel::findAll();
-        $arrLog = [];
+        $offset = 0;
+        $batch = 50000;
 
-        if(null !== $objLog)
-        {
-            while($objLog->next())
-            {
-                $arrRow = [];
-
-                $arrRow['id'] = $objLog->id;
-                $arrRow['config'] = $objLog->cid;
-                $arrRow['version'] = $objLog->version;
-                $arrRow['date'] = date(Config::get('dateFormat'), $objLog->tstamp);
-                $arrRow['time'] = date(Config::get('timeFormat'), $objLog->tstamp);
-                $arrRow['domain'] = $objLog->domain;
-                $arrRow['url'] = $objLog->url;
-                $arrRow['ip'] = $objLog->ip;
-
-                $arrServices = StringUtil::deserialize($objLog->config);
-                $arrConfig = [];
-
-                if(null !== $arrServices)
-                {
-                    $x = 1;
-                    foreach($arrServices as $item)
-                    {
-                        $arrConfig[] = $x . '. ' . $item['title'] . ' (ID: ' . $item['id'] . ', Cookie-Names: ' . ($item['token'] ?: '-') . ')';
-                        $x++;
-                    }
-                }
-
-                $arrRow['confirmed'] =  implode("\n", $arrConfig);
-                $arrLog[] = $arrRow;
-            }
-        }
-
-        $serializer = new Serializer([new ObjectNormalizer()], [new CsvEncoder()]);
-        $strData = $serializer->encode($arrLog, 'csv', [
-            'csv_delimiter' => ';'
-        ]);
+        $dateFormat = $this->convertToSQLDateFormat(Config::get('dateFormat')); // d.m.Y
+        $timeFormat = $this->convertToSQLDateFormat(Config::get('timeFormat')); // H:i
 
         $objFile = new File('system/tmp/' . md5(uniqid(mt_rand(), true)));
-        $objFile->write($strData);
+        $objFile->write('');
         $objFile->close();
 
+        $realPath = System::getContainer()->getParameter('kernel.project_dir') . '/' . $objFile->path;
+
+        $handle = fopen($realPath, 'w');
+
+        if (false === $handle)
+        {
+            // Maybe fall back to memory extensive array in this case?
+            $objFile->write('Could not download the consent log due to missing write permissions within system/tmp');
+            $objFile->close();
+            $objFile->sendToBrowser('consent-log-error.txt');
+        }
+
+        $header = ['id', 'config', 'version', 'date', 'time', 'domain', 'url', 'ip', 'confirmed'];
+
+        fputcsv($handle, $header, ';');
+
+        $db = Database::getInstance();
+
+        do {
+            $result = $db->execute("SELECT
+                    `id`, `cid` as config, `version`,
+                    FROM_UNIXTIME(tstamp, '".$dateFormat."') as date,
+                    FROM_UNIXTIME(tstamp, '".$timeFormat."') as time,
+                    domain, url, ip,
+                    config as confirmed
+                FROM tl_cookie_log
+                LIMIT $offset, $batch"
+            );
+
+            $data = $result->fetchAllAssoc();
+
+            if (!empty($data))
+            {
+                foreach ($data as $row)
+                {
+                    if (!empty($row['confirmed']))
+                    {
+                        $arrServices = StringUtil::deserialize($row['confirmed']);
+                        $arrConfig = [];
+
+                        if (null !== $arrServices)
+                        {
+                            foreach ($arrServices as $i => $item)
+                            {
+                                $arrConfig[] = ($i + 1) . '. ' . $item['title'] . ' (ID: ' . $item['id'] . ', Cookie-Names: ' . ($item['token'] ?: '-') . ')';
+                            }
+                        }
+
+                        $row['confirmed'] = implode("\n", $arrConfig);
+                    }
+
+                    fputcsv($handle, $row, ';');
+                }
+            }
+
+            $offset += $batch;
+        }
+        while (
+            !empty($data)
+        );
+
+        fclose($handle);
+
         $objFile->sendToBrowser('consent-log.csv');
+    }
+
+    /**
+     * Convert the PHP time format syntax to mysql and mariadb syntax
+     * https://www.php.net/manual/en/datetime.format.php#refsect1-datetime.format-parameters
+     * https://mariadb.com/kb/en/from_unixtime/
+     */
+    private function convertToSQLDateFormat(string $format): string
+    {
+        $formatMapping = [
+            'd'=>'%d','D'=>'%a','j'=>'%e','l'=>'%W','N'=>'%u','S'=>'','w'=>'%w','z'=>'%j', // Day
+            'W'=>'%V', // Week
+            'F'=>'%M','m'=>'%m','M'=>'%b','n'=>'%c', // Month
+            'Y'=>'%Y','y'=>'%y', // Year
+            'a'=>'%p','A'=>'%p','g'=>'%l','G'=>'%k','h'=>'%h','H'=>'%H','i'=>'%i','s'=>'%s','u'=>'%f', // Time
+            'e'=>'','I'=>'','O'=>'','P'=>'','T'=>'','Z'=>'', // Timezone
+        ];
+
+        return strtr($format, $formatMapping);
     }
 }
