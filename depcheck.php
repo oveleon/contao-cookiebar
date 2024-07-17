1b6db090e5a1f6de9d5bfc7b12c5da4cc56c3bf3
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-07-17 17:59:03 +0200

    Add dep-check analyzer

---
+++ depcheck.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->ignoreErrorsOnPackage('contao/manager-plugin', [ErrorType::DEV_DEPENDENCY_IN_PROD])
+;
