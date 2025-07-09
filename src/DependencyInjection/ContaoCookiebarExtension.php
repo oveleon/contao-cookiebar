ddc145d5f68a4cbeb3913c047b5f3716f35bb491
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-07-09 10:48:36 +0200

    Match the return type with the `ExtensionInterface`

--- src/DependencyInjection/ContaoCookiebarExtension.php
+++ src/DependencyInjection/ContaoCookiebarExtension.php
@@ -18,7 +18,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class ContaoCookiebarExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $config = $this->processConfiguration(new Configuration(), $configs);
 
