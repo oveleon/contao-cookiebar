8237bed62f9ca9782bcbd0c5558e66bcdaa1be1e
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-08-20 16:44:50 +0200

    Drop gulp in favor of webpack encore

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('./public/')
+    .setPublicPath(Encore.isDevServer() ? '/public/' : '/bundles/contaocookiebar/')
+    .setManifestKeyPrefix('')
+
+    .cleanupOutputBeforeBuild()
+    .disableSingleRuntimeChunk()
+    .enableBuildNotifications()
+
+    .addEntry('config', './assets/scripts/config-presets.js')
+    .addEntry('cookiebar', './assets/scripts/cookiebar.js')
+
+    .addEntry('default', './assets/default.js')
+    .addEntry('simple', './assets/simple.js')
+
+    .enablePostCssLoader()
+    .enableVersioning(Encore.isProduction())
+
+    .configureDevServerOptions((options) => Object.assign({}, options, {
+        static: false,
+        hot: true,
+        liveReload: true,
+        allowedHosts: 'all',
+        watchFiles: ['assets/**/*', 'contao/**/*'],
+        client: {
+            overlay: false
+        }
+    }));
+
+module.exports = Encore.getWebpackConfig();
