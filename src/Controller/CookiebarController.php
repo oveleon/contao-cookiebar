3db413e25204c9547e88ef6481357d857858a2b7
Christian Mette <13376233+cmette@users.noreply.github.com>
2026-01-28 17:27:41 +0100

    Update src/Controller/CookiebarController.php
    Co-authored-by: Fritz Michael Gschwantner <fmg@inspiredminds.at>

--- src/Controller/CookiebarController.php
+++ src/Controller/CookiebarController.php
@@ -26,7 +26,6 @@ use Oveleon\ContaoCookiebar\Utils\TwigRenderTrait;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
